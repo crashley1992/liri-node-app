28bca40aa604db352cba1f3ff3462bd3f3d62940
Ashley Clarke <crashley92@gmail.com>
2019-10-02 17:40:35 -0700

    beautified code

--- liri.js
+++ liri.js
@@ -19,114 +19,115 @@ console.log(process.argv.slice(3).join(" "));
 begin(command, userInput);
 
 
-    function begin(command, userInput) {
-                switch(command) {
-                case "concert-this":
-                concertThis(userInput);
-                    break;
+function begin(command, userInput) {
+  switch (command) {
+    case "concert-this":
+      concertThis(userInput);
+      break;
 
-                case "spotify-this-song":
-                    spotifyThis(userInput);
-                    break;
-                
-                case "movie-this":
-                        movieThis(userInput);
-                    break;
+    case "spotify-this-song":
+      spotifyThis(userInput);
+      break;
 
-                case "do-what-it-says":
-                    doWhatItSays();
-                    break;
+    case "movie-this":
+      movieThis(userInput);
+      break;
 
-                    default:
-                        break;
-                }
-            }
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
 
-            //function for the concert input
-            function concertThis(userInput){
-                    //if statement to let user response be input if theres no input
-                    if (userInput === "") {
-                        userInput = "Beck";
-                    }
-                    //AXIOS YOO
-                    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(function (response) {
-                        
-                    var concertResults = response.data[0];
-                    console.log(concertResults);
+    default:
+      break;
+  }
+}
 
-                    //moment variable
-                    var momentTime = moment(concertResults.datetime).format("MM/DD/YYYY");
+//function for the concert input
+function concertThis(userInput) {
+  //if statement to let user response be input if theres no input
+  if (userInput === "") {
+    userInput = "Beck";
+  }
+  //AXIOS YOO
+  axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(function(response) {
 
-                    console.log("\nVenue Name: " + concertResults.venue.name + "\nVenue Location: " + concertResults.venue.city + "\nDate of Event: " + momentTime);
-                });
-            };
+    var concertResults = response.data[0];
+    console.log(concertResults);
 
-    //Spotify this song input
-            function spotifyThis(userInput) {
-                //for no user input
-                    if (userInput === "") {
-                        userInput = "The Sign";
-                    }
+    //moment variable
+    var momentTime = moment(concertResults.datetime).format("MM/DD/YYYY");
 
-                    spotify.search( { type: 'track', query: userInput }, function(err, data) {
+    console.log("\nVenue Name: " + concertResults.venue.name + "\nVenue Location: " + concertResults.venue.city + "\nDate of Event: " + momentTime);
+  });
+};
 
-                        if (err) {
-                          return console.log(err);
+//Spotify this song input
+function spotifyThis(userInput) {
+  //for no user input
+  if (userInput === "") {
+    userInput = "The Sign";
+  }
 
-                        } else {
-                          for (var i = 0; i < data.tracks.items.length; i++ ) {
-                    
-                          console.log("Artist: " + data.tracks.items[i].artists[0].name);
-                          console.log("Song Name: " + data.tracks.items[i].name);
-                          console.log("Album Name: " + data.tracks.items[i].album.name + "\n");
-                          };
-                        }
-                       
-                      });
-                    
-                    };
-                    
-                    
-                //Movie name input
+  spotify.search({
+    type: 'track',
+    query: userInput
+  }, function(err, data) {
 
-                function movieThis(userInput) {
-                    if (userInput === "") {
-                        //will put superstar as default if it's left blank
-                        userInput = "Superstar"; 
-                    }
+    if (err) {
+      return console.log(err);
 
-                    axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(function (response) {
+    } else {
+      for (var i = 0; i < data.tracks.items.length; i++) {
 
-                        // Variable for concert results
-                        var movieResults = response.data;
-                        // console.log(movieResults);
-                        console.log("Movie Title: " + movieResults.Title);
-                        console.log("Release Year: " + movieResults.Year);
-                        console.log("IMDB Rating: " + movieResults.imdbRating);
-                        console.log("Rotten Tomatoes Rating: " + movieResults.Ratings[1].Value);
-                        console.log("Country: " + movieResults.Country);
-                        console.log("Language: " + movieResults.Language);
-                        console.log("Plot: " + movieResults.Plot);
-                        console.log("Actors: " + movieResults.Actors + "\n");
-                      
-                        });
-                      };
+        console.log("Artist: " + data.tracks.items[i].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[i].name);
+        console.log("Album Name: " + data.tracks.items[i].album.name + "\n");
+      };
+    }
+
+  });
+
+};
 
 
-                //do what it says input from radnom.txt
-                function doWhatItSays(){
-                    fs.readFile("random.txt", "UTF8", function (err, data) {
-                      
-                      // Console logs error if any
-                      if (err) {
-                      return console.log(err);
-                      }
-                  
-                      // Splits data between comma
-                      var dataArr = data.split(",")
-                      console.log(dataArr);
-                      begin(dataArr[0], dataArr[1]);
-                    })
-                  }
+//Movie name input
 
-            
+function movieThis(userInput) {
+  if (userInput === "") {
+    //will put superstar as default if it's left blank
+    userInput = "Superstar";
+  }
+
+  axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(function(response) {
+
+    // Variable for concert results
+    var movieResults = response.data;
+    // console.log(movieResults);
+    console.log("Movie Title: " + movieResults.Title);
+    console.log("Release Year: " + movieResults.Year);
+    console.log("IMDB Rating: " + movieResults.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + movieResults.Ratings[1].Value);
+    console.log("Country: " + movieResults.Country);
+    console.log("Language: " + movieResults.Language);
+    console.log("Plot: " + movieResults.Plot);
+    console.log("Actors: " + movieResults.Actors + "\n");
+
+  });
+};
+
+
+//do what it says input from radnom.txt
+function doWhatItSays() {
+  fs.readFile("random.txt", "UTF8", function(err, data) {
+
+    // Console logs error if any
+    if (err) {
+      return console.log(err);
+    }
+
+    // Splits data between comma
+    var dataArr = data.split(",")
+    console.log(dataArr);
+    begin(dataArr[0], dataArr[1]);
+  })
+}
